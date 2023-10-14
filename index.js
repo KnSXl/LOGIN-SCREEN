be1e1b93ebb0c05aa7810d659f9fa7c1ecad3808
Kauã Nascimento <kauasilva2861>
2023-10-14 17:55:05 -0300

    Login Screen template

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ function login() {
     //recupera os dados de cadastro do localStorage
     const users = JSON.parse(localStorage.getItem('users')) || [];
 
+    //teste se o campo não esta vazia ou incompleto
+    if (email === "" && senha === "") {
+        document.getElementById('result').innerHTML = "Cadastro falhou, preencha o campo"
+        
+    }
+    else if (senha.length < 8 || email.length < 8){
+        document.getElementById('result').innerHTML = "Cadastro incompleto, tente novamente (mínimo 8 caracteres)"
+    }
+    else{
+
     //verifica se o email e senha corresponde a um usuário
     const user = users.find(u => u.email === email && u.senha === senha);
 
-    if(user){
-        document.getElementById('result').innerHTML = 'Login bem-sucedido';
-    } else {
-        document.getElementById('result').innerHTML = 'Login falhou';
-    }
+        if(user){
+            document.getElementById('result').innerHTML = 'Login bem-sucedido';
+        } else {
+            document.getElementById('result').innerHTML = 'Login falhou';
+        }
+
+    }  
+
 }
 
 function register() {
@@ -24,6 +37,18 @@ function register() {
     //recupera os dados de cadastro do localStorage
     const users = JSON.parse(localStorage.getItem('users')) || [];
 
+    //teste se o campo não esta vazia ou incompleto
+    if (email === "" && senha === "") {
+        document.getElementById('result').innerHTML = "Cadastro falhou, preencha o campo"
+        
+    }
+
+    else if (senha.length < 8 || email.length < 8){
+        document.getElementById('result').innerHTML = "Cadastro incompleto, tente novamente (mínimo 8 caracteres)"
+    }
+
+    else{
+
     //registra o email e a senha no banco de dados
     users.push({email, senha})
 
@@ -31,4 +56,7 @@ function register() {
     localStorage.setItem('users', JSON.stringify(users))
 
     document.getElementById('result').innerHTML = 'Cadastro bem-sucedido'
+
+    }
+
 }
